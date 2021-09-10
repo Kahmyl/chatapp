bc28368cd95a7e9296be0a086e1a9f1dab309b24
Kahmyl <59609015+Kahmyl@users.noreply.github.com>
2021-09-10 10:30:04 +0100

    n

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Cruizers | Kahmyl</title>
-  <link style="height: 50px; width: 50px;" rel="shortcut icon" type="image/x-icon" href="cool.png">
+  <link style="height: 50px; width: 50px;" rel="shortcut icon" type="image/x-icon" href="public/images/cool.png">
   <link rel="stylesheet" href="public/css/style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
 </head>
